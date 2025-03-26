ea6bbf77b67bc669e3cb82ab736448ce38888f4f
NikhilThakur3536 <thakurnikhil657@gmail.com>
2025-03-26 21:30:49 +0530

    First Animation

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { SignUp } from "./components/signup"
+
+
+function App() {
+
+
+
+  return (
+    <div>Helo</div>
+  )
+}
+
+export default App
+
+
---
+++ src/components/bubbleanimationbg.tsx
@@ -0,0 +1,12 @@
+import { AnimatedBubble } from "../ui/animatedBubble";
+import { bubbleDataArr } from "../data/animatedBubbleData";
+
+export const AnimatedBubbleBg = () => {
+  return (
+    <div className=" relative top-0 w-full h-full">
+      {bubbleDataArr.map((items, index) => (<AnimatedBubble key={index} icon={items.icon} />))}
+    </div>
+  )
+}
+
+
